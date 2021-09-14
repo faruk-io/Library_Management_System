7a1575bd5460aaf13db1c348ea187aeb649e9fb8
omar faruk <faruk141pau@gmail.com>
2021-09-14 23:18:52 +0600

    Initial commit

---
+++ Controllers/IssueBookController.cs
@@ -0,0 +1,56 @@
+﻿using LibraryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class IssueBookController : Controller
+    {
+        nitlibraryEntities db = new nitlibraryEntities();
+        // GET: IssueBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Getbook()
+        {
+            var books = db.books.Select(p => new
+            {
+
+                ID = p.id,
+                Bname = p.bname
+
+            }).ToList();
+            return Json(books, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public ActionResult GetMid(int mid)
+        {
+            var memberid = (from s in db.members where s.id == mid select s.name).ToList();
+            return Json(memberid, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Save(issuebook issue)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                db.issuebooks.Add(issue);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(issue);
+        }
+     }
+
+    
+}
---
+++ Controllers/ReturnBookController.cs
@@ -0,0 +1,54 @@
+﻿using LibraryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class ReturnBookController : Controller
+    {
+        nitlibraryEntities db = new nitlibraryEntities();
+
+        // GET: ReturnBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(returnbook retruns)
+        {
+            if (ModelState.IsValid)
+            {
+                db.returnbooks.Add(retruns);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(retruns);
+        }
+
+
+
+        public ActionResult GetMid(int mid)
+        {
+            var memberid = (from s in db.issuebooks
+                            where s.m_id == mid
+                            select new
+                            {
+                                IssueDate = s.issuedate,
+                                ReturnDate = s.returndate,
+                                Memberid = s.m_id,
+                                BookName = s.book_id,
+                                ElapsedDays = SqlFunctions.DateDiff("day", s.returndate, DateTime.Now)
+
+                            }).ToArray();
+                            
+                            
+
+            return Json(memberid, JsonRequestBehavior.AllowGet);
+        }
+    }
+}  
